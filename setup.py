366c2877fc7688fd3ab2945fdd9b2bb05fcda33b
Haoming Li <61410145+haoming-codes@users.noreply.github.com>
2025-10-31 16:57:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,11 +23,12 @@ setup(
         "dragonmapper",
         "sktime",
         "openai",
-        "num2words",
+        # "num2words",
         "tqdm",
         "pandas",
         "python-calamine",
         "abydos @ git+https://github.com/denizberkin/abydos.git",
+        "num2words @ git+https://github.com/savoirfairelinux/num2words.git"
     ],
     python_requires=">=3.8",
 )
